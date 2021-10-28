ef1cc24e60c719f93206e01e036f240140ede8a7
Kenny <kenny.mrsk@gmail.com>
2021-10-28 18:00:54 -0400

    A1

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routerApi = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server en express');
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Nueva ruta');
+})
+
+routerApi(app);
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
