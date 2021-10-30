1abc369ff77a04038e138dfe2d9e1cfc13bcad73
Kenny <kenny.mrsk@gmail.com>
2021-10-30 18:29:08 -0400

    A3

--- routes/products.router.js
+++ routes/products.router.js
@@ -3,11 +3,12 @@ const faker = require('faker');
 
 const router = express.Router();
 
+// READ
 router.get('/', (req, res) => {
   const products = [];
-  const { size } = req.query;
+  const {size} = req.query;
   const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
+  for (let i = 0; i < limit; i++) {
     products.push({
       name: faker.commerce.productName(),
       price: parseInt(faker.commerce.price(), 10),
@@ -17,25 +18,47 @@ router.get('/', (req, res) => {
   res.json(products);
 });
 
-router.get('/filter', (req, res) => {
+/* router.get('/filter', (req, res) => {
   res.send('FIlter')
-});
+}); */
 
 router.get('/:id', (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
   res.json({
     id,
-    name: 'Product 2',
-    price: 2000
+    name: faker.commerce.productName(),
+    price: parseInt(faker.commerce.price(), 10),
+    image: faker.image.imageUrl(),
   });
 });
 
+// CREATE
 router.post('/', (req, res) => {
   const body = req.body;
   res.json({
     message: 'created',
     data: body
   });
-})
+});
+
+// UPDATE
+router.patch('/:id', (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  const {id} = req.params
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
 
 module.exports = router;
--- routes/users.router.js
+++ routes/users.router.js
@@ -3,16 +3,58 @@ const faker = require('faker');
 
 const router = express.Router();
 
+// READ users
 router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
+  const users = [];
+  const {size} = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    users.push({
+      name: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      image: faker.image.imageUrl(),
     });
-  } else {
-    res.send('No hay parametros');
   }
-})
+  res.json(users)
+});
+
+router.get('/:id', (req,res) => {
+  const {id} = req.params;
+  res.json({
+    id,
+    name: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    image: faker.image.imageUrl(),
+  });
+});
+
+// CREATE
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'Created',
+    data: body
+  });
+});
+
+// UPDATE
+router.patch('/:id', (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update',
+    data: body,
+    id,
+  });
+});
+
+// DELETE
+router.delete('/:id', (req, res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'Deleted',
+    id,
+  });
+});
 
 module.exports = router;
