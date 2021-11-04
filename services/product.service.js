44ece8a3248272344e3586cea605b332e7566a59
Kenny <kenny.mrsk@gmail.com>
2021-11-04 15:24:11 -0400

    A7

--- services/product.service.js
+++ services/product.service.js
@@ -1,6 +1,37 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
 
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(15);
+const price = Joi.number().integer().min(10);
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+function validatorHadler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const {error} = schema.validate(data);
+    if (error) {
+      next(boom.badRequest(error));
+    }
+    next;
+  }
+}
+
 class ProductsService {
 
   constructor() {
