50a3212b722abd74e9208cb7b050ff16290af536
Kenny <kenny.mrsk@gmail.com>
2021-12-13 23:06:57 -0400

    133/12/2021

--- pass-hash.js
+++ pass-hash.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');;
+const bcrypt = require('bcrypt');
 
 async function hashPassword() {
   const myPassword = 'admin 123 .202';
--- pass-verify.js
+++ pass-verify.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');;
 
 async function verifyPassword() {
   const myPassword = 'admin 123 .202';
-  const hash = '$2b$10$acqv2gMkTkmXvlArRfOZGu/aSeP5tI9Nn2.EohAxRiVPU3kh/M0OO'
+  const hash = '$2b$10$9yLCgLBOM1w1LjYglr2.JOmcM9IAwxJ42eoZN2i6/rbjqJEuAIwGG'
   const isMatch = await bcrypt.compare(myPassword, hash);
   console.log(isMatch);
 }
